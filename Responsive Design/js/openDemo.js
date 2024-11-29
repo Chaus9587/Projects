b9518dc0d4b371d7c1052adfa3b0c7ac9ec3d018
Chaus <151802765+Chaus9587@users.noreply.github.com>
2024-11-29 17:55:12 +0800

    Add files via upload

---
+++ Responsive Design/js/openDemo.js
@@ -0,0 +1,19 @@
+// Open demo webpage
+
+function openMediaDemo(id) {
+    window.open(`../tutorials/@media/demo${id}.html`, "output", "width=750, height=400"); 
+}
+
+function openFlexFdDemo(id) {
+    window.open(`../tutorials/flexbox/flex-direction/demo${id}.html`, "output", "width=850, height=600"); 
+}
+
+function openFlexFwDemo(id) {
+    window.open(`../tutorials/flexbox/flex-wrap/demo${id}.html`, "output", "width=850, height=600"); 
+}
+
+function openRelDemo(id) {
+    window.open(`../tutorials/relative units/demo${id}.html`, "output", "width=850, height=600");
+}
+
+
